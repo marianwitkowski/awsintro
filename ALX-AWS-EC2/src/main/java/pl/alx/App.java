311dc6987f61258d0bfb764ddabe299a7daeaa54
Marian Witkowski <marian.witkowski@gmail.com>
2021-12-14 08:44:10 +0100

    Add files via upload

---
+++ ALX-AWS-EC2/src/main/java/pl/alx/App.java
@@ -0,0 +1,49 @@
+package pl.alx;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static AWSCredentials aws_cred = new AWSCredentials() {
+        @Override
+        public String getAWSAccessKeyId() {
+            return "AKIAY3OL7L2FN6GHCKU2";
+        }
+
+        @Override
+        public String getAWSSecretKey() {
+            return "AGifbw3UJjks79HbzIikHU5/xxZS3goHDzjOkYL7";
+        }
+    };
+
+    public static void main( String[] args )
+    {
+
+        AmazonEC2 ec2 = AmazonEC2ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(aws_cred))
+                .withRegion(Regions.EU_WEST_1)
+                .build();
+
+        RunInstancesRequest request_ec2 = new RunInstancesRequest()
+                .withImageId("ami-04dd4500af104442f")
+                .withInstanceType(InstanceType.T1Micro)
+                .withMaxCount(1).withMinCount(1);
+
+        RunInstancesResult result = ec2.runInstances(request_ec2);
+        String instanceId = result.getReservation().getInstances().get(0).getInstanceId();
+        System.out.println(instanceId);
+
+    }
+}
